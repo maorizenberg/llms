1b238a5e2dc24fa58b096ecc2bd9631c7c5d21df
Maor Izenberg <maor.izenberg@orca-ai.io>
2025-04-01 15:46:40 +0300

    new excercise

---
+++ create_brochure.py
@@ -0,0 +1,118 @@
+import json
+import re
+import requests
+from bs4 import BeautifulSoup
+from rich.console import Console
+from rich.markdown import Markdown
+import ollama
+
+OLLAMA_API = "http://localhost:11434/api/chat"
+HEADERS = {"Content-Type": "application/json"}
+MODEL = "llama3.2"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+
+
+class Website:
+
+    def __init__(self, url):
+        """
+        Create this Website object from the given url using the BeautifulSoup library
+        """
+        self.url = url
+        response = requests.get(url, headers=headers)
+        self.body = response.content
+        soup = BeautifulSoup(self.body, "html.parser")
+        self.title = soup.title.string if soup.title else "No title found"
+        if soup.body:
+            for irrelevant in soup.body(["script", "style", "img", "input"]):
+                irrelevant.decompose()
+            self.text = soup.body.get_text(separator="\n", strip=True)
+        else:
+            self.text = ""
+        links = [link.get("href") for link in soup.find_all("a")]
+        self.links = [link for link in links if link]
+
+    def get_contents(self):
+        return f"Webpage Title:\n{self.title}\nWebpage Contents:\n{self.text}\n\n"
+
+
+def get_links_user_prompt(website):
+    user_prompt = f"Here is the list of links on the website of {website.url} - "
+    user_prompt += "please decide which of these are relevant web links for a brochure about the company, respond with the full https URL in JSON format. \
+Do not include Terms of Service, Privacy, email links.\n"
+    user_prompt += "Links (some might be relative links):\n"
+    user_prompt += "\n".join(website.links)
+    return user_prompt
+
+
+def get_links(url):
+    website = Website(url)
+    link_system_prompt = "You are provided with a list of links found on a webpage. \
+    You are able to decide which of the links would be most relevant to include in a brochure about the company, \
+    such as links to an About page, or a Company page, or Careers/Jobs pages.\n"
+    link_system_prompt += "You should respond in JSON as in this example:"
+    link_system_prompt += """
+        {
+            "links": [
+                {"type": "about page", "url": "https://full.url/goes/here/about"},
+                {"type": "careers page": "url": "https://another.full.url/careers"}
+            ]
+        }
+        """
+    print('Use ollama to get relevant links')
+    response = ollama.chat(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": link_system_prompt},
+            {"role": "user", "content": get_links_user_prompt(website)},
+        ],
+    )
+    result = response["message"]["content"]
+    # Skip the prefix of result and get only the data
+    match = re.search(r'({.*})', result, re.DOTALL)
+    if match:
+        json_str = match.group(1)
+        return json.loads(json_str)
+    else:
+        print("No JSON found in the text.")
+        return {}
+
+
+def get_all_details(url):
+    result = "Landing page:\n"
+    result += Website(url).get_contents()
+    links = get_links(url)
+    print(links)
+    for link in links["links"]:
+        result += f"\n\n{link['type']}\n"
+        result += Website(link["url"]).get_contents()
+    return result
+
+def get_brochure_user_prompt(company_name, url):
+    user_prompt = f"You are looking at a company called: {company_name}\n"
+    user_prompt += "Here are the contents of its landing page and other relevant pages; use this information to build a short brochure of the company in markdown.\n"
+    user_prompt += get_all_details(url)
+    user_prompt = user_prompt[:5_000] # Truncate if more than 5,000 characters
+    return user_prompt
+
+def create_brochure(company_name, url):
+    system_prompt = "You are an assistant that analyzes the contents of several relevant pages from a company website \
+and creates a short brochure about the company for prospective customers, investors and recruits. Respond in markdown.\
+Include details of company culture, customers and careers/jobs if you have the information."
+    print('use ollama to create a brochure')
+    response = ollama.chat(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": get_brochure_user_prompt(company_name, url)},
+        ],
+    )
+    result = response["message"]["content"]
+    console = Console()
+    console.print(Markdown(result))
+
+
+create_brochure("HuggingFace", "https://huggingface.co")
