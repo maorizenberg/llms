4cf2bdb798738a74ef5c67e5f17b0f6609e1d69b
Maor Izenberg <maor.izenberg@orca-ai.io>
2025-04-01 15:46:31 +0300

    print nicely

--- summarize_webpage.py
+++ summarize_webpage.py
@@ -1,17 +1,17 @@
-import os
 import requests
-from dotenv import load_dotenv
 from bs4 import BeautifulSoup
-from IPython.display import Markdown, display
-from openai import OpenAI
+from rich.console import Console
+from rich.markdown import Markdown
+import ollama
 
 OLLAMA_API = "http://localhost:11434/api/chat"
 HEADERS = {"Content-Type": "application/json"}
 MODEL = "llama3.2"
 
 headers = {
- "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 }
+
 
 class Website:
 
@@ -21,39 +21,38 @@ class Website:
         """
         self.url = url
         response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
         self.title = soup.title.string if soup.title else "No title found"
         for irrelevant in soup.body(["script", "style", "img", "input"]):
             irrelevant.decompose()
         self.text = soup.body.get_text(separator="\n", strip=True)
 
-        system_prompt = "You are an assistant that analyzes the contents of a website \
-and provides a short summary, ignoring text that might be navigation related. \
-Respond in markdown."
 
-def user_prompt_for(website):
-    user_prompt = f"You are looking at a website titled {website.title}"
+def generate_user_prompt(website_title: str, website_text: str) -> str:
+    user_prompt = f"You are looking at a website titled {website_title}"
     user_prompt += "\nThe contents of this website is as follows; \
 please provide a short summary of this website in markdown. \
 If it includes news or announcements, then summarize these too.\n\n"
-    user_prompt += website.text
+    user_prompt += website_text
     return user_prompt
 
-    def messages_for(website):
-    return [
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": user_prompt_for(website)}
-    ]
 
-    import ollama
-
-def summarize(url):
+def display_summary(url):
+    print(f'Scarping {url}')
     website = Website(url)
-    response = ollama.chat(model=MODEL, messages=messages_for(website))
-    return response['message']['content']
+    system_prompt = "You are an assistant that analyzes the contents of a website \
+        and provides a short summary, ignoring text that might be navigation related. \
+        Respond in markdown."
 
-    def display_summary(url):
-    summary = summarize(url)
-    display(Markdown(summary))
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": generate_user_prompt(website.title, website.text)},
+    ]
+    print('Getting summary')
+    response = ollama.chat(model=MODEL, messages=messages)
+    summary = response["message"]["content"]
+    console = Console()
+    console.print(Markdown(summary))
 
-    display_summary("https://edwarddonner.com")
+
+display_summary("https://imdb.com")
